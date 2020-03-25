4cb0c5357428695d1178c77e69d64899f46e8fce
skybear308 <skybearok@outlook.com>
2020-03-25 14:14:31 -0500

    initial upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <Bar />
+      <v-content>
+        <PasswordGenerator />
+      </v-content>
+      <Footer />
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Bar from './components/Bar';
+import Footer from './components/Footer';
+import PasswordGenerator from './components/PasswordGenerator';
+
+export default {
+  name: 'App',
+
+  components: {
+    Bar,
+    Footer,
+    PasswordGenerator,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
